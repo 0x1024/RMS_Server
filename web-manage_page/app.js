194d1f5c1792f3890845149aee3a2439386c2649
1 <1@1.com>
2017-04-11 11:34:59 +0800

    the base func test done, first update add web page interface

---
+++ web-manage_page/app.js
@@ -0,0 +1,94 @@
+﻿document.getElementById("pid").value="10000"
+document.getElementById("dtype").value="新机型"
+document.getElementById("client").value="研发"
+document.getElementById("tags").value="设计中"
+document.getElementById("passwd").value="123"
+document.getElementById("created").value=robot.getDate("-")+" "+robot.getTime() ;
+document.getElementById("updated").value=robot.getDate("-")+" "+robot.getTime() ;
+
+var mysocket = new WebSocket("ws://10.1.11.151:9003");
+	mysocket.onopen=function(){
+		console.log("连接成功");
+		init_req();
+
+	}
+  mysocket.onmessage = function(data) {
+ 	  		console.log(data);
+     var msg = JSON.parse(data.data);
+      var data=msg.data;
+      // if (msg.cmd == "data_single") {
+      //     var contStr = "";
+      //     for (var i = 0; i < data.length; i++) {
+      //         contStr += "<tr><td>" + data[i].Pid + "</td><td>" + data[i].Dtype + "</td><td>" + data[i].Client + "</td><td>" + data[i].Tags + "</td><td>" + data[i].Passwd + "</td><td>" + data[i].Created + "</td><td>" + data[i].Updated + "</td></tr>";
+      //     }
+      //     document.getElementById("tcont").innerHTML = contStr;
+      // }
+
+      switch(msg.cmd){
+          case "data_single":
+          var contStr = "";
+  
+              contStr += "<tr><td>" + data.Pid + "</td><td>" + data.Dtype + "</td><td>" + data.Client + "</td><td>" + data.Tags + "</td><td>" + data.Passwd + "</td><td>" + data.Created + "</td><td>" + data.Updated + "</td></tr>";
+
+          document.getElementById("tcont").innerHTML = contStr;
+          break;
+
+           case "HB":
+			ack_HB()
+          break;
+
+          case "data_all":
+          var contStr = "";
+          for (var i = 0; i < data.length; i++) {
+              contStr += "<tr><td>" + data[i].Pid + "</td><td>" + data[i].Dtype + "</td><td>" + data[i].Client + "</td><td>" + data[i].Tags + "</td><td>" + data[i].Passwd + "</td><td>" + data[i].Created + "</td><td>" + data[i].Updated + "</td></tr>";
+          }
+          document.getElementById("tcont").innerHTML = contStr;
+          break;
+          default:
+          break;
+
+     
+      }
+
+  }
+
+function ack_HB(){
+  	  var sentdata={};
+  	  sentdata.cmd="HB";
+      mysocket.send(JSON.stringify(sentdata));
+}
+
+  function req(){
+  	  var sentdata={};
+  	  sentdata.cmd="req";
+	  sentdata.pid=document.getElementById("Req ID").value;
+     	 mysocket.send(JSON.stringify(sentdata));
+}
+
+  function delete_id(){
+  	  var sentdata={};
+  	  sentdata.cmd="delete_id";
+	  sentdata.pid=document.getElementById("delete_id").value;
+     	 mysocket.send(JSON.stringify(sentdata));
+}
+
+  function init_req(){
+  	  var sentdata={};
+  	  sentdata.cmd="all";
+
+     	 mysocket.send(JSON.stringify(sentdata));
+}
+
+  function submit(){
+  	  var sentdata={};
+ 	  sentdata.cmd="comitone";		
+  	  sentdata.pid=document.getElementById("pid").value;
+  	  sentdata.dtype=document.getElementById("dtype").value;
+  	  sentdata.client=document.getElementById("client").value;
+  	  sentdata.tags=document.getElementById("tags").value;
+  	  sentdata.passwd=document.getElementById("passwd").value;
+  	  sentdata.created=document.getElementById("created").value;
+  	  sentdata.updated=document.getElementById("updated").value;
+  	  
+      mysocket.send(JSON.stringify(sentdata));
+  }
---
+++ web-manage_page/login.js
@@ -0,0 +1,100 @@
+﻿
+
+
+
+var mysocket = new WebSocket("ws://10.1.11.151:9003");
+document.getElementById("ack").value=  JSON.stringify(mysocket.status)	
+
+
+mysocket.onopen=function(){
+		console.log("ws建立连接");
+	}	
+mysocket.onclose=function(){
+		console.log("ws关闭连接");
+	}	
+	
+mysocket.onmessage = function(data) {
+ 	  		console.log(new Date(),data);
+     var msg = JSON.parse(data.data);
+      var data=msg.data;
+
+      switch(msg.cmd){
+          case "auth_ok":
+			stayHome=0;
+			window.location.href="index.html";
+          break;
+		  
+          case "auth_failed":
+
+  			document.getElementById("ack").value='登陆异常';
+        break;
+		  
+           case "auth_name_fault":
+ 			document.getElementById("ack").value='用户名不存在';
+         break;
+
+           case "auth_pwd_fault":
+			document.getElementById("ack").value='密码错误';
+          break;
+		  
+			default:
+			document.getElementById("ack").value='什么鬼！！';
+
+           case "HB":
+			ack_HB()
+          break;
+		  
+          break;
+
+     
+      }
+
+}	
+	
+//comment	
+ function login(){
+	if(mysocket){	 
+		var username=document.getElementById("user").value;
+		var password=document.getElementById("pswd").value;
+		
+			
+		if (username=="") {
+			document.getElementById("ack").value='用户名不能空';
+		}else if (password=="") {
+		   document.getElementById("ack").value='密码不能空'; 
+		}else{
+			
+		
+		auth_req();
+
+		}
+	}
+	
+};
+
+
+
+	
+
+
+
+  
+function ack_HB(){
+  	  var sentdata={};
+  	  sentdata.cmd="HB";
+      mysocket.send(JSON.stringify(sentdata));
+}
+
+
+function auth_req(){
+	if(mysocket){
+  	  var sentdata={};
+  	  sentdata.cmd="auth_req";
+	  sentdata.user=document.getElementById("user").value;
+	  sentdata.pswd=document.getElementById("pswd").value;
+	  
+     	 mysocket.send(JSON.stringify(sentdata));
+		 }
+}
+
+
